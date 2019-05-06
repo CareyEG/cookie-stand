1b54ae4a9a1b9c06f4378916a17ce0086b5ad15b
Toshi <careyeg@gmail.com>
2019-05-06 16:11:42 -0700

    its been a while

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ var Pike = {
   maxhrly: 65,
   avgcookies: 6.3,
   introduction: function(){
-    console.log(+ this.name + 's hourly minimum cookies sold was ' + this.minhrly + '. It\'s hourly maximum cookies sold was ' + this.maxhrly + ', and the average cookie sales were calculated to ' + this.avgcookies + '.');
+    console.log(+ this.name + 's hourly minimum customers was ' + this.minhrly + '. It\'s hourly maximum customers was ' + this.maxhrly + ', and the average cookie sales were calculated to ' + this.avgcookies + '.');
+    function getRandomArbitrary(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;}
+      
+    getRandomArbitrary(23, 65) * (6.3);
   },
 };
 
@@ -16,7 +22,13 @@ var Airport = {
   maxhrly: 24,
   avgcookies: 1.2,
   introduction: function(){
-    console.log(+ this.name + 's hourly minimum cookies sold was ' + this.minhrly + '. It\'s hourly maximum cookies sold was ' + this.maxhrly + ', and the average cookie sales were calculated to ' + this.avgcookies + '.');
+    console.log(+ this.name + 's hourly minimum customers was ' + this.minhrly + '. It\'s hourly maximum customers was ' + this.maxhrly + ', and the average cookie sales were calculated to ' + this.avgcookies + '.');
+    function getRandomArbitrary(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;}
+      
+    getRandomArbitrary(3, 24) * (1.2);
   },
 };
 
@@ -26,7 +38,13 @@ var Center = {
   maxhrly: 38,
   avgcookies: 3.7,
   introduction: function(){
-    console.log(+ this.name + 's hourly minimum cookies sold was ' + this.minhrly + '. It\'s hourly maximum cookies sold was ' + this.maxhrly + ', and the average cookie sales were calculated to ' + this.avgcookies + '.');
+    console.log(+ this.name + 's hourly minimum customers was ' + this.minhrly + '. It\'s hourly maximum customers was ' + this.maxhrly + ', and the average cookie sales were calculated to ' + this.avgcookies + '.');
+    function getRandomArbitrary(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;}
+      
+    getRandomArbitrary(11, 38) * (3.7);
   },
 };
 
@@ -36,7 +54,13 @@ var Hill = {
   maxhrly: 38,
   avgcookies: 2.3,
   introduction: function(){
-    console.log(+ this.name + 's hourly minimum cookies sold was ' + this.minhrly + '. It\'s hourly maximum cookies sold was ' + this.maxhrly + ', and the average cookie sales were calculated to ' + this.avgcookies + '.');
+    console.log(+ this.name + 's hourly minimum customers was ' + this.minhrly + '. It\'s hourly maximum customers was ' + this.maxhrly + ', and the average cookie sales were calculated to ' + this.avgcookies + '.');  
+    function getRandomArbitrary(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;}
+      
+    getRandomArbitrary(20, 38) * (2.3);
   },
 };
 
@@ -46,12 +70,20 @@ var Alki = {
   maxhrly: 16,
   avgcookies: 4.6,
   introduction: function(){
-    console.log(+ this.name + 's hourly minimum cookies sold was ' + this.minhrly + '. It\'s hourly maximum cookies sold was ' + this.maxhrly + ', and the average cookie sales were calculated to ' + this.avgcookies + '.');
+    console.log(+ this.name + 's hourly minimum customers was ' + this.minhrly + '. It\'s hourly maximum customers was ' + this.maxhrly + ', and the average cookie sales were calculated to ' + this.avgcookies + '.');
+    function getRandomArbitrary(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;}
+    
+    getRandomArbitrary(2, 16) * (4.6);
   },
 };
+
 
 var stores = [Pike, Airport, Center, Hill, Alki];
 
 for (var i = 0; i < stores.length; i++) {
-  console.log(stores[i].name)
+  console.log(stores[i].name);
 }
+
