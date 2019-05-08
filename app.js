5af7b8fc7dc7de5dce5672b80103c404deb48986
Toshi <careyeg@gmail.com>
2019-05-08 00:42:53 -0700

    added the non-functional table...

--- app.js
+++ app.js
@@ -205,7 +205,93 @@ function calcRandomCustomers(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/* To see what we need to do to build a HTML table with JS, let's take a look at the HTML needed to start the table and make the top two rows:
+<table>             PUT THIS AND ONLY THIS IN THE HTML, WITH AN ID
+  <tr>              create tr
+  <th>Name</th>   create a th, give it content, append it to tr
+  <th>Color</th>  create a th, give it content, append it to tr
+  <th>Tail</th>   create a th, give it content, append it to tr
+  </tr>             append tr to the table
+  <tr>
+    <td>Buddy</td>
+    <td>Orange and White</td>
+    <td>VERY long</td>
+  </tr>
+</table>
+Tables are built in the same way that a typewriter types: left-to-right, top-to-bottom.
+Thus, we'll want to create and append HTML elements in the same order.
+We have already put an empty <table> element in the HTML, with id="cats" */
 
+// We need an array to hold our cats
+var allHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+// We need to access the table that is in the DOM
+var locations = document.getElementById('locations');
+
+// We need a constructor to make our cat objects
+function Location(name, cookies) {
+  this.name = name;
+  this.cookies = cookies;
+  this.render = function () {
+    // make a tr
+    var trEl = document.createElement('tr');
+    // create, content, append for "Name"
+    var tdEl = document.createElement('td');
+    tdEl.textContent = this.name;
+    trEl.appendChild(tdEl);
+    // create, content, append for "Color"
+    tdEl = document.createElement('td');
+    tdEl.textContent = this.cookies;
+    trEl.appendChild(tdEl);
+    // create. content, append for "Tail Size"
+    // append the tr to the table
+    locations.appendChild(trEl);
+  };
+  allHours.push(this);
+}
+
+// We need to create our Cat instances
+new Location('1st and Pike', '');
+new Location('SeaTac Airport', '');
+new Location('Seattle Center', '');
+new Location('Capitol Hill', '');
+new Location('Alki', '');
+
+// console.table(allCats);
+
+// We need a separate function to make the table header
+
+function makeHeaderRow() {
+  // create the row
+  var trEl = document.createElement('tr');
+  // create, content, append first cell
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Name';
+  trEl.appendChild(thEl);
+  // create, content, append second cell
+  thEl = document.createElement('th');
+  thEl.textContent = 'Cookies';
+  trEl.appendChild(thEl);
+  // create, content, append third cell
+  // append the row to the table
+  Location.appendChild(trEl);
+}
+
+
+
+// It would be nice to have a single function that renders all of the individual cat rows
+function renderallHours() {
+  for (var i = 0; i < allHours.length; i++) {    
+    allHours[i].render();
+  }
+}
+
+// Now we need to call our functions: the one for the header row, and the one for generating the individual cat rows
+
+makeHeaderRow();
+renderallHours();
+
+// Don't forget in the Chrome dev tools to observe the difference between the HTML shown in the Sources tab versus the Elements tab!
 
 
 
